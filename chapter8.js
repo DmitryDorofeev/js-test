69c3464f97e253408432d0cc730736a9ba4d1e26
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2014-11-09 00:45:07 +0300

    функции

--- chapter8.js
+++ chapter8.js
@@ -5,7 +5,7 @@ var f = function faa(x) {
 	else {
 		return x*faa(x - 1);
 	}
-}
+};
 
 console.log(f(3));
 
@@ -19,6 +19,7 @@ function max()
     }
     return m;
 }
+
 var largest = max(1, 10, 100, 2, 3, 1000, 4, 5, 10000, 6);
 
 console.log(largest);
@@ -34,3 +35,106 @@ function sum(a) {
     }
     else throw new Error("sum(): аргумент должен быть массивом"); 
 }
+
+
+function operate(operator, operand1, operand2) {
+    return operator(operand1, operand2);
+}
+
+
+var operators = {
+        add:      function(x,y) { return x+y; },
+        subtract: function(x,y) { return x - y; },
+        multiply: function(x,y) { return x*y; },
+        divide:   function(x,y) { return x/y; },
+        pow:      Math.pow
+};
+
+function operate(op_name, operand1, operand2) {
+    if (typeof operators[op_name] == "function") {
+        return operators[op_name](operand1, operand2); 
+    }
+    else {
+        throw "неизвестный оператор";
+    }
+}
+var j = operate("add", "hello", operate("add", " ", "world"));
+var k = operate("pow", 10, 2);
+
+console.log(j, k);
+
+function check(args) {
+    var actual = arguments.length; // в книге args.length, видимо, очепятка
+    var expected = arguments.callee.length; // тут тоже
+    if (actual != expected) {
+        throw new Error("неверное число аргументов: ожидается: " + expected + "; фактически передано " + actual);
+    }
+}
+
+check('lol');
+// check('мент', 'аргумент'); // тут вызывается нужный эксепшн, ну и молодец
+
+function getPropertyNames(o) {
+    var r = [];
+    for (var name in o) { // чувак вары в цикле в книге не пишет)
+        r.push(name);
+    }
+    return r;
+}
+
+function copyProperties(from,  to) {
+    to = to || {}; // упростил
+    for(var p in from) {
+        to[p] = from[p];
+    }
+    return to;
+}
+
+function copyUndefinedProperties(from, to) {
+    for(var p in from) {
+        if (p in to) {
+            to[p] = from[p];
+        }
+    }
+}
+
+var sss = {prop1: 890, hhh: 7},
+    aaa = {l: 'q'};
+
+console.log(copyProperties(sss, aaa));
+
+function bindMethod(o, f) { // видимо так работает _.bind
+    return function() { 
+        return f.apply(o, arguments); 
+    };
+}
+
+var x = "глобальная";
+    function z () {
+        var x = "локальная";
+        function g () { console.log(x); }
+        g();
+}
+z();
+
+function makeProperty(o, name, predicate) {
+    var value;
+    o["get" + name] = function() { return value; };
+
+    o["set" + name] = function(v) {
+        if (predicate && !predicate(v)) {
+            throw "set" + name + ": неверное значение " + v;
+        }
+        else {
+           value = v; 
+        }
+    }; 
+}
+var o = {};
+makeProperty(o, "Name", function(x) { return typeof x == "string"; });
+o.setName("Frank");
+o.setName("Frank2");
+console.log(o.getName()); // в книге тут print(); веселые опечаточки
+// o.setName(0); 
+
+
